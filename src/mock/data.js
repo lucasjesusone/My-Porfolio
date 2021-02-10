e54a34ed1262acba30b256ebf513198a77ca441e
lucasjesusone <lucasjesusone@gmail.com>
2021-02-10 01:50:03 -0400

    #3 Code Adjust

--- src/mock/data.js
+++ src/mock/data.js
@@ -43,10 +43,11 @@ export const projectsData = [
     id: nanoid(),
     img: 'page.png',
     title: 'Landing Page',
-    info: 'This Landing Page is being built for a personal project with Gatsby / ReactJS and CSS.',
+    info:
+      'This Landing Page is being built for a personal project with Javascript - ReactJS and CSS. Still in development',
     info2: 'You can check all details in the repo below.',
     url: '',
-    repo: 'https://github.com/LucasJesusone/React-Landing-Page', // if no repo, the button will not show up
+    repo: 'https://github.com/LucasJesusone/landing-page', // if no repo, the button will not show up
   },
 ];
 
